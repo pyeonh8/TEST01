cae5849d4e68df4c6c4b9f0a0cc29c60d71b6471
박연희 <pyeonh8@gmail.com>
2025-06-28 15:20:28 +0900

    First commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/product/:id',
+        destination: '/items/:id',
+        permanent: true,
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      new URL(
+        'https://learn-codeit-kr-static.s3.ap-northeast-2.amazonaws.com/codeitmall/**'
+      ),
+    ],
+  },
 };
 
 export default nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,30 @@
-import "@/styles/globals.css";
+import '@/styles/global.css';
+import Header from '@/components/Header';
+import Container from '@/components/Container';
+import { ThemeProvider } from '@/lib/ThemeContext';
+import Head from 'next/head';
+import { Noto_Sans_KR } from 'next/font/google';
+
+const notoSansKR = Noto_Sans_KR({
+  weight: ['400', '700'],
+  subsets: [],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <style>{`html {font-family: ${notoSansKR.style.fontFamily}, sans-serif;}`}</style>
+        <title>Codeitmall</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <ThemeProvider>
+        <Header />
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+      </ThemeProvider>
+    </>
+  );
 }
